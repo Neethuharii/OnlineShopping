7fff59367644bf92ac4251811b7808e39f823c10
DESKTOP-JV8UP02\Livares <neethu.hari@livares.com>
2024-04-04 18:29:52 +0530

    Exception Handler

---
+++ src/main/java/exception/CustomException.java
@@ -0,0 +1,54 @@
+package exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomException extends RuntimeException{
+
+	    private static final long serialVersionUID = 1L;
+	    private HttpStatusCode httpStatus;
+	    private Integer errorCode;
+
+	    public CustomException(String message) {
+	        super(message);
+	    }
+
+	    public CustomException(Integer error, String message) {
+	        super(message);
+	        this.errorCode = error;
+	        this.httpStatus = HttpStatus.valueOf(error);
+	    }
+
+	    public CustomException(Integer error, Exception ex) {
+	        super(ex);
+	        this.errorCode = error;
+	        this.httpStatus = HttpStatus.valueOf(error);
+	    }
+
+	    public CustomException(Exception ex) {
+	        super(ex);
+	        this.errorCode = ErrorCode.INTERNAL_SERVER_ERROR.getCode();
+	        this.httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+	    }
+
+	    public CustomException(ErrorCode errorCode, String message) {
+	        this(errorCode.getCode(), message);
+	    }
+	    
+	    public CustomException(HttpStatusCode status, ErrorCode errorCode, String message) {
+	        this(errorCode, message);
+	        this.httpStatus = status;
+	    }
+
+	    public CustomException(ErrorCode errorCode, Exception ex) {
+	        this(errorCode.getCode(), ex);
+	    }
+
+		
+	}
+
